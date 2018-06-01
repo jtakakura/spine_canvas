6d451910a30af7cc095ea0560426c71f1988d3fe
Junji Takakura <j.takakura@gmail.com>
2018-06-02 07:14:00 +0900

    Initial commit

---
+++ example/spine_canvas_example.dart
@@ -0,0 +1,6 @@
+import 'package:spine_canvas/spine_canvas.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/spine_canvas.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library spine_canvas;
+
+export 'src/spine_canvas_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
